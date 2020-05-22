197e59d40a6d46d0048b7e6ea762935decc2842f
Donata Dietz <dietzdonata@gmail.com>
2020-05-22 12:19:21 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,35 +1,30 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail
-  def new
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render 'cocktails/show'
     end
   end
 
   def destroy
-    # 1. Find the record
-    # 2. Destroy the record
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    # 3. Redirect
-    redirect_to doses_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:cocktail_id, :description, :ingredient)
-  end
-
-  def find_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
